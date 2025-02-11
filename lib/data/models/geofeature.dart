dbabfa9b209b92339aedc5e627a8bfca0ab855d5
MaisLaufen <vip.eftifanov@mail.ru>
2025-02-11 18:27:05 +0700

    [Remove] unused property in feature type

--- lib/data/models/geofeature.dart
+++ lib/data/models/geofeature.dart
@@ -2,9 +2,8 @@ import 'package:geoapp/data/models/geometry.dart';
 
 /// Класс для представления объекта GeoJSON
 class GeoFeature {
-  final String type;
   final Geometry geometry;
   final Map<String, dynamic> properties;
 
-  GeoFeature({required this.type, required this.geometry, required this.properties});
+  GeoFeature({required this.geometry, required this.properties});
 }
